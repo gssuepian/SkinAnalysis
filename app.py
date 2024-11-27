5949aab9c13dd80f52e05f17f237fe1efa551ec3
Siriyakorn S. <gssuepian@gmail.com>
2024-11-28 00:02:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ import markdown
 ## Initialize our streamlit app
 st.set_page_config(page_title="Skin & Makeup Analysis App", page_icon=':sparkles:')
 
+# Use local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+local_css("style/style.css")
+
 st.image("images/maintitle.png")
 
 menu = ["Home", "Skin Analysis", "The Future"]
